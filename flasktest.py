a6908a3faabbefaf0eb50f927a767e38e598743c
Mehmet Erkılıç <Mehmet@172-30-6-74.dynapool.nyu.edu>
2011-10-01 21:48:27 -0400

    When Howie left.

--- flasktest.py
+++ flasktest.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 
+import json
+import csv
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['DEBUG'] = True
+
 @app.route('/<name>')
 def hello(name):
 	return render_template('index.html', name=name)
+
+@app.route('/howie/<name>')
+def howie(name):
+	return render_template('index.html', name=name)
+
+@app.route('/banks/<state>')
+def taxicomplaints(state):
+	f = open('banklist.csv')
+	rdr = csv.reader(f)
+	banks = [row for row in rdr if row[2] == state]
+	return render_template('banks.html', state=state,  banks=banks)
 
 if __name__ == "__main__":
 	app.run()
